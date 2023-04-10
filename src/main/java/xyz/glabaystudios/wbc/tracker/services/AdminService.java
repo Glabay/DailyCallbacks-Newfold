a22b22b77c80c453b18f34be31f202c384427284
Glabay <java_guru@live.com>
2023-04-10 11:16:22 -0300

    April 10th, 2023 - Admin Filters
    Notes:
    - Adds the ability to search for callbacks between a provided date range
    
    Signed-off-by: Glabay <Java_Guru@live.com>

--- src/main/java/xyz/glabaystudios/wbc/tracker/services/AdminService.java
+++ src/main/java/xyz/glabaystudios/wbc/tracker/services/AdminService.java
@@ -20,7 +20,24 @@ public class AdminService {
             }
             // Check if the Specific Agent field is populated
             else if (Objects.nonNull(filter.getFilterByAgent()) && !filter.getFilterByAgent().isBlank()) {
+                // if the specific agent has a callback, add it to the list
                 if (Objects.equals(filter.getFilterByAgent(), cb.getAgent())) filteredCallbacks.add(cb);
+                // if the specific agent has a callback and we're looking for complete or incomplete
+                if (Objects.nonNull(filter.getFilterByStatus()) && !filter.getFilterByStatus().isBlank()) {
+                    if (Objects.equals(filter.getFilterByStatus().toUpperCase(),"COMPLETE")) {
+                        // Looking for completed cases
+                        if (Objects.equals(cb.getCompleted(), Boolean.TRUE)) filteredCallbacks.add(cb);
+                    }
+                    if (Objects.equals(filter.getFilterByStatus().toUpperCase(),"INCOMPLETE")) {
+                        // Looking for incomplete cases
+                        if (Objects.equals(cb.getCompleted(), Boolean.FALSE)) filteredCallbacks.add(cb);
+                    }
+                }
+            }
+            // Check if the start date is not null or empty
+            else if (Objects.nonNull(filter.getFilterStartDay()) && !filter.getFilterStartDay().isBlank()) {
+                if (isCallbacksBetweenDates(filter.getFilterStartDay(), filter.getFilterEndDay(), cb.getDateOfCallback()))
+                    filteredCallbacks.add(cb);
             }
             // Check if the Completed Status field is populated
             else if (Objects.nonNull(filter.getFilterByStatus()) && !filter.getFilterByStatus().isBlank()) {
@@ -36,4 +53,29 @@ public class AdminService {
         }
         return filteredCallbacks;
     }
+
+
+    private boolean isCallbacksBetweenDates(String startingDate, String endingDate, String callbackDate) {
+        int startingDateYear = Integer.parseInt(startingDate.split("-")[0]);
+        int startingDateMonth = Integer.parseInt(startingDate.split("-")[1]);
+        int startingDateDay = Integer.parseInt(startingDate.split("-")[2]);
+
+        int endingDateYear = Integer.parseInt(endingDate.split("-")[0]);
+        int endingDateMonth = Integer.parseInt(endingDate.split("-")[1]);
+        int endingDateDay = Integer.parseInt(endingDate.split("-")[2]);
+
+        int callbackYear = Integer.parseInt(callbackDate.split("-")[0]);
+        int callbackMonth = Integer.parseInt(callbackDate.split("-")[1]);
+        int callbackDay = Integer.parseInt(callbackDate.split("-")[2]);
+
+        // Are we within the year
+        if (callbackYear >= startingDateYear && callbackYear <= endingDateYear)
+            // Are we within the Month range
+            if (callbackMonth >= startingDateMonth && callbackMonth <= endingDateMonth)
+                // are we within the Day Range
+                return (callbackDay >= startingDateDay && callbackDay <= endingDateDay);
+            else return false;
+        else return false;
+    }
+
 }
